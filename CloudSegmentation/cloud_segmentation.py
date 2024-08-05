fix to iou calculation

CloudSegmentation/cloud_segmentation.py
@@ -18,12 +18,12 @@ class CloudSegmentation(pl.LightningModule):
         self.save_hyperparameters()
 
         # self.model = UNet(num_channels, num_classes)
-        # self.model = ResNetUNet(num_channels, num_classes)
-        self.model = CSDNet(num_channels, num_classes)
+        self.model = ResNetUNet(num_channels, num_classes)
+        # self.model = CSDNet(num_channels, num_classes)
         self.activation = nn.Softmax(dim=1) if num_classes > 1 else None
 
         # self.loss = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
-        self.loss = CSDNetLoss(1)
+        # self.loss = CSDNetLoss(1)
 
     def forward(self, x):
         out = self.model(x)
@@ -37,11 +37,11 @@ class CloudSegmentation(pl.LightningModule):
 
         predictions = self(images)
         if self.hparams.num_classes == 1:
-            # loss = F.binary_cross_entropy_with_logits(predictions, labels[:, None, :, :].float())
-            loss = self.loss(predictions, labels)
+            loss = F.binary_cross_entropy_with_logits(predictions, labels[:, None, :, :].float())
+            # loss = self.loss(predictions, labels)
         else:
             ...
-        iou = binary_iou(predictions[0], labels)
+        iou = binary_iou(predictions, labels)
 
         self.log_dict({
             "train_loss": loss,
@@ -53,11 +53,11 @@ class CloudSegmentation(pl.LightningModule):
         images, labels = batch
         predictions = self(images)
         if self.hparams.num_classes == 1:
-            # loss = F.binary_cross_entropy_with_logits(predictions, labels[:, None, :, :].float())
-            loss = self.loss(predictions, labels)
+            loss = F.binary_cross_entropy_with_logits(predictions, labels[:, None, :, :].float())
+            # loss = self.loss(predictions, labels)
         else:
             ...
-        iou = binary_iou(predictions[0], labels)
+        iou = binary_iou(predictions, labels)
         if stage:
             self.log_dict({
                 f"{stage}_loss": loss,
@@ -71,7 +71,8 @@ class CloudSegmentation(pl.LightningModule):
         self._evaluate(batch, 'test')
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=self.hparams.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1.0e-2)
+        return optimizer
 
 
 
@@ -81,7 +82,7 @@ if __name__ == "__main__":
     from torch.utils import data
     from pathlib import Path
 
-    ds = Cloud38(Path('/home/av/data'), True, None, image_size=(192, 192))
+    ds = Cloud38(Path('/home/av/data'), True, None, image_size=(192, 192), )
 
     train_set_size = int(len(ds) * 0.8)
     valid_set_size = len(ds) - train_set_size

CloudSegmentation/metrics.py
@@ -8,17 +8,20 @@ def accuracy(preds, labels):
 
 
 @torch.no_grad()
-def binary_iou(preds, labels):
+def binary_iou(outputs: torch.Tensor, labels: torch.Tensor):
     SMOOTH = 1e-6
-    preds = torch.sigmoid(preds) > 0.5
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    outputs = torch.sigmoid(outputs) > 0.5
+    intersection = (outputs & labels.long()).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels.long()).float().sum((1, 2))  # Will be zzero if both are 0
 
-    intersection = (preds & labels.long()).float().sum((2, 3))
-    union = (preds | labels.long()).float().sum((2, 3))
-    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
 
-    thresholded = torch.clamp(20 * (iou-0.5), 0, 10).ceil() / 10
-    return thresholded.mean()
-
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    return thresholded.mean()  # Or thresholded.mean() if you are interested in average across the batch
 
 @torch.no_grad()
 def binary_dice_coefficient(preds, labels, eps=1e-5):